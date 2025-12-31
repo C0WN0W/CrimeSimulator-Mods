f32019727a1c2554d0b3b43c7d31ce309b9db800
C0WN0W <imcownow@yeah.net>
2025-12-31 19:48:25 +0800

    Add files via upload

---
+++ No Police.cs
@@ -0,0 +1,26 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("com.cownow.nopolice", "No Police", "1.0.0")]
+public class noPolicePatch : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        var harmony = new Harmony("com.cownow.nopolice");
+        harmony.PatchAll();
+        Logger.LogInfo("No Police MOD loaded.");
+    }
+}
+
+// Still using this shit Class
+[HarmonyPatch(typeof(ThermalVision), "Update")]
+class ThermalVisionUpdatePatch
+{
+    static void Postfix(ThermalVision __instance)
+    {
+        if (__instance.player.WM.policeManager != null)
+            __instance.player.WM.policeManager.currentChaseLevel = 0;
+            
+    }
+}
