3d762b316ffd4862e9eeaf5766dda4504e80f0f9
C0WN0W <imcownow@yeah.net>
2026-01-03 21:21:06 +0800

    Add files via upload

---
+++ Blowtorch Unlocker.cs
@@ -0,0 +1,28 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("com.cownow.blowtorch", "Blowtorch Unlocker", "1.0.0")]
+public class BlowtorchPatchPlugin : BaseUnityPlugin
+{
+    void Awake()
+    {
+        var harmony = new Harmony("com.cownow.blowtorch");
+        harmony.PatchAll();
+        Logger.LogInfo("Blowtorch Patch loaded.");
+    }
+}
+
+[HarmonyPatch(typeof(BlowtorchObject), "AddDamage")]
+class Blowtorch
+{
+    static bool Prefix(BlowtorchObject __instance)
+    {
+        if (!__instance.destroyed)
+        {
+            __instance.RPC_DestroyLock();
+        }
+        return false;
+    }
+
+}
