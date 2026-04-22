76bc0b65189904d7108f27bafccd0786f158b848
C0WN0W <imcownow@yeah.net>
2026-04-23 01:45:29 +0800

    Update Alchemy.cs

--- Alchemy.cs
+++ Alchemy.cs
@@ -1,19 +1,50 @@
-﻿using BepInEx;
+using BepInEx;
+using BepInEx.Configuration;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace CN_Alchemy
 {
-    [BepInPlugin("com.cownow.furnace", "Alchemy Mod", "1.0.0")]
-    public class FurnaceAlwaysGoldPlugin : BaseUnityPlugin
+    [BepInPlugin("CN_Alchemy", "JewelryFurnaceMod", "1.0.1")]
+    public class JewelryFurnaceMod : BaseUnityPlugin
     {
+        public static ConfigEntry<float> BurnDuration;
+
         private void Awake()
         {
             var harmony = new Harmony("com.cownow.furnace");
             harmony.PatchAll();
+
+            BurnDuration = Config.Bind("General", "BurnDuration", 10f, "Burn Duration");
+
             Logger.LogInfo("Alchemy Mod Loaded.");
         }
+        public static float BurnDurationValue => BurnDuration.Value;
     }
+
+
+    [HarmonyPatch(typeof(JewelryFurnace), "Update")]
+    class JewelryFurnace_Update_Patch
+    {
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            for (int i = 0; i < codes.Count; i++)
+            {
+                if (codes[i].opcode == OpCodes.Ldc_R4 && (float)codes[i].operand == 10f)
+                {
+                    codes[i] = new CodeInstruction(OpCodes.Call,
+                        AccessTools.PropertyGetter(typeof(JewelryFurnaceMod), nameof(JewelryFurnaceMod.BurnDurationValue)));
+                }
+            }
+
+            return codes;
+        }
+    }
+
 
     [HarmonyPatch(typeof(JewelryFurnace), "CheckItems")]
     public static class JewelryFurnace_CheckItems_Patch
@@ -39,4 +70,3 @@ namespace CN_Alchemy
 
 
 }
-
